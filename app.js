c2ef8fb783d28b560b4394361917704fa0fdd9e5
bngu10 <ba.nguyen@nike.com>
2018-04-09 20:53:42 -0700

    current

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    mongoose = require('mongoose');
 
-var port = process.env.PORT || 3000;
+var app = express();
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel'); //translate mongodb to obj
+
+var port = process.env.PORT || 3000; //look for port env if not set to 3000
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+  .get(function(req, res){
+
+    var query = {}; //json filter based on what is returned?
+    if (req.query.genre){
+      query.genre = req.query.genre;
+    }
+    Book.find(function(err, books){
+      if (err)
+        console.log(err);
+      else {
+        res.json(books);
+      }
+    })
+  })
 
 app.get('/', function (req, res){
   res.send('Welcome to my API');
