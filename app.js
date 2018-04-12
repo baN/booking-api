8226ffd2c2a8b6d1767c2f25037280ab8e2bd1f7
bngu10 <ba.nguyen@nike.com>
2018-04-11 19:32:32 -0700

    posted

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+   mongoose = require('mongoose'),
+   bodyParser = require('body-parser');
 
-var app = express();
 var db = mongoose.connect('mongodb://localhost/bookAPI');
-var Book = require('./models/bookModel'); //translate mongodb to obj
-
+var Book = require('./models/bookModel');
+var app = express();
 var port = process.env.PORT || 3000; //look for port env if not set to 3000
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
 bookRouter.route('/Books')
-  .get(function(req, res){
+  .post(function(req,res){ //add new data
+    var book = new Book(req.body); //body parser translates to json
 
-    var query = {}; //json filter based on what is returned?
-    if (req.query.genre){
-      query.genre = req.query.genre;
-    }
-    Book.find(function(err, books){
-      if (err)
-        console.log(err);
-      else {
-        res.json(books);
-      }
-    })
+    book.save();
+    res.status(201).send(book);
   })
+  .get(function(req,res){
 
-app.get('/', function (req, res){
-  res.send('Welcome to my API');
+     var query = {};
+
+     if (req.query.genre){
+       query.genre = req.query.genre;
+     }
+
+      Book.find(function(err,books) {
+         if(err)
+            res.status(500).send(err);
+         else
+            res.json(books);
+      });
+   });
+
+bookRouter.route('/Books/:bookId')
+  .get(function(req,res){
+
+     Book.findById(req.params.bookId, function(err, book) {
+        if(err)
+           res.status(500).send(err);
+        else
+           res.json(book);
+     });
+  });
+
+app.use('/api', bookRouter);
+app.get('/', function(req, res) {
+ res.send('Welcome to my API!');
 });
-
 app.listen(port, function() {
-  console.log('listening on port' + port);
+ console.log('Gulp is running my app on PORT: ' + port);
 });
