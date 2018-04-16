f01384059157635dee999af52c9dfe57d3d0d7f5
bngu10 <ba.nguyen@nike.com>
2018-04-16 09:38:58 -0700

    Adding controllers.

--- app.js
+++ app.js
@@ -7,47 +7,15 @@ var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000; //look for port env if not set to 3000
 
-bookRouter = require('./Routes/bookRoutes');
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
-bookRouter.route('/Books')
-  .post(function(req,res){ //add new data
-    var book = new Book(req.body); //body parser translates to json
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-    book.save();
-    res.status(201).send(book);
-  })
-  .get(function(req,res){
+app.use('/api/books', bookRouter);
+app.use('/api/authors', bookRouter);
 
-     var query = {};
-
-     if (req.query.genre){
-       query.genre = req.query.genre;
-     }
-
-      Book.find(function(err,books) {
-         if(err)
-            res.status(500).send(err);
-         else
-            res.json(books);
-      });
-   });
-
-bookRouter.route('/Books/:bookId')
-  .get(function(req,res){
-
-     Book.findById(req.params.bookId, function(err, book) {
-        if(err)
-           res.status(500).send(err);
-        else
-           res.json(book);
-     });
-  });
-
-app.use('/api', bookRouter);
 app.get('/', function(req, res) {
  res.send('Welcome to my API!');
 });
